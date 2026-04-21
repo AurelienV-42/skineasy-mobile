83a7b38453b375c427a4ee16a4cb9c68e54b0898
Aurélien Vandaële <aurelienvpro@gmail.com>
2026-04-21 22:20:12 +0200

    chore: preview eas and bump

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: process.env.APP_NAME ?? 'Skin Easy',
   slug: 'skineasy',
-  version: '1.0.1',
+  version: '1.0.2',
   orientation: 'portrait',
   icon: './assets/icon.png',
   userInterfaceStyle: 'light',
